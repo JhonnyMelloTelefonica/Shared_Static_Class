728e23972117b4adf6faba0973aa754461f14490
AdrielFnd <adriel.paula@telefonica.com>
2024-04-25 15:14:04 -0300

    atualizações dos models do filtro de gerenciamento de usuários;

--- Model/PaginationControleAcessoModel.cs
+++ Model/PaginationControleAcessoModel.cs
@@ -6,19 +6,19 @@ namespace Shared_Static_Class.Models
     {
         public int PageNumber { get; set; }
         public int PageSize { get; set; }
-        public string? Nome { get; set; }
+        public string Nome { get; set; }
 
         //public string? MatriculaDivisao { get; set; }
-        public string MatriculaDivisao { get; set; }
-        public int? Matricula { get; set; }
-        public string? Pdv { get; set; }
-        public string? email { get; set; }
-        public bool? IsSuporte { get; set; }
-        public List<string>? Cargo { get; set; }
-        public List<string>? Canal { get; set; }
-        public List<string>? Regional { get; set; }
-        public List<string>? Uf { get; set; }
-        public List<bool>? Fixa { get; set; }
+        public List<string> MatriculaDivisao { get; set; }
 
+        public int? Matricula { get; set; }
+        public string Pdv { get; set; }
+        public string email { get; set; }
+        public bool? IsSuporte { get; set; }
+        public List<double> Cargo { get; set; }
+        public List<double> Canal { get; set; }
+        public List<string> Regional { get; set; }
+        public List<string> Uf { get; set; }
+        public List<bool> Fixa { get; set; }
     }
 }
