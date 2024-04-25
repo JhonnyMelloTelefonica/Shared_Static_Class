6110d081dd988fd2de269cc16922ff6b4785ad41
JHONNY GLAUCIO NASCIMENTO DE MELO <jhonny.melo@telefonica.com>
2024-04-25 20:32:57 -0300

    Alterações para sexta

--- Model/DETALHADO_ACESSO_PENDENTE_MODEL.cs
+++ Model/DETALHADO_ACESSO_PENDENTE_MODEL.cs
@@ -1,13 +1,14 @@
 ﻿using Shared_Static_Class.Converters;
 using Shared_Static_Class.Data;
 using Shared_Static_Class.Enums;
+using Shared_Static_Class.Model_DTO;
 
 namespace Shared_Static_Class.Models
 {
     public class DETALHADO_ACESSO_PENDENTE_MODEL
     {
         public SOLICITACAO_USUARIO_DETALHADO SOLICITACAO { get; set; }
-        public ACESSOS_MOBILE? ACESSOS_MOBILE { get; set; } = null;
+        public ACESSOS_MOBILE_DTO? ACESSOS_MOBILE { get; set; } = null;
         public IEnumerable<MENSAGEM_ACESSO_PENDENTE> RESPOSTAS { get; set; } = [];
     }
 }
