8bf1cbc0cb63142e746a6bccde649f3c8db090da
JHONNY GLAUCIO NASCIMENTO DE MELO <jhonny.melo@telefonica.com>
2024-07-11 10:46:35 -0300

    - Finalização de index em COntrole de Demandas; - Inicio de Desing em abertura de demanda; - Correção de erros gerais em aplicação de controle de demandas;

---
+++ Converters/TimerPlus.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared_Static_Class.Converters
+{
+    public class TimerPlus : System.Timers.Timer
+    {
+        private DateTime m_dueTime;
+
+        public TimerPlus() : base() => this.Elapsed += this.ElapsedAction;
+        protected new void Dispose()
+        {
+            this.Elapsed -= this.ElapsedAction;
+            base.Dispose();
+        }
+        public TimeSpan TimeLeft => (this.m_dueTime - DateTime.Now);
+        public new void Start()
+        {
+            this.m_dueTime = DateTime.Now.AddMilliseconds(this.Interval);
+            base.Start();
+        }
+
+        private void ElapsedAction(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (this.AutoReset)
+                this.m_dueTime = DateTime.Now.AddMilliseconds(this.Interval);
+        }
+    }
+}
