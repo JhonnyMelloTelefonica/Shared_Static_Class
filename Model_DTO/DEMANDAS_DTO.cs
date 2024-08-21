3c2ed47723d67c9cce96bd2220e20b0f36d0e9eb
JHONNY GLAUCIO NASCIMENTO DE MELO <jhonny.melo@telefonica.com>
2024-08-21 16:57:38 -0300

    Correções de visualização Vivo X (Minas); Correção de visualização de gerenciamento de usuário (Gerente); Alteração de coluna regional DEMANDA_RELACAO_CHAMADO;

--- Model_DTO/DEMANDAS_DTO.cs
+++ Model_DTO/DEMANDAS_DTO.cs
@@ -194,6 +194,7 @@ namespace Shared_Static_Class.Model_DTO
         public Tabela_Demanda Tabela { get; set; }
         public string tipo => Tabela.GetDisplayName();
         public string LastStatus { get; set; }
+        public string REGIONAL { get; set; } = string.Empty;
         public DateTime DATA_ABERTURA { get; set; }
         public int MATRICULA_SOLICITANTE { get; set; }
         public int MATRICULA_RESPONSAVEL { get; set; }
