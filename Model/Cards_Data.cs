2a358f2ec41f27496b882536abb284728029f244
JHONNY GLAUCIO NASCIMENTO DE MELO <jhonny.melo@telefonica.com>
2024-08-27 12:05:56 -0300

    -Tela inicial Vivo X: Separar no OneDrive as pastas de MG e NE para as imagens. Também separar as visões no código, ou seja, quem é do NE só poderá visualizar as imagens do NE e a mesma coisa com o MG.; -Tipo fila ainda aparece mesmo com todas as sub-filas desativadas; -Responsáveis Em filtros de COntrole de filas sem separação por regional; -Editar Sub-Fila com erro; -problema em navegação; -Busca de Responsáveis incorreta; -Correção de atualização de status da demanda em painel de chamados;

---
+++ Model/Cards_Data.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared_Static_Class.Model
+{
+    public class Cards_data
+    {
+        [JsonProperty("$content-type")]
+        public string content_type { get; set; }
+        [JsonProperty("$content")]
+        public string content { get; set; }
+        [JsonIgnore]
+        public string contentBytes
+        {
+            get
+            {
+                return "data:image/png;base64," + content;
+            }
+        }
+    }
+}
