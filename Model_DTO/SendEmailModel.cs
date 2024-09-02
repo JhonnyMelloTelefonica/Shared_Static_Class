22bb32ba2e116a5a989e69c274382aeb8f9cd2c4
JHONNY GLAUCIO NASCIMENTO DE MELO <jhonny.melo@telefonica.com>
2024-09-02 16:26:24 -0300

    Adcionar NOva Fila com Carregamento de Tela infinito

---
+++ Model_DTO/SendEmailModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared_Static_Class.Model_DTO
+{
+    public class SendEmailModel
+    {
+        public IEnumerable<string> Para { get; set; } = [];
+        public string De { get; set; } = "";
+        public string Assunto { get; set; } = string.Empty;
+        public string HtmlCorpo { get; set; } = string.Empty;
+        public IEnumerable<string> CC { get; set; } = [];
+        public IEnumerable<string> Anexos { get; set; } = [];
+    }
+}
