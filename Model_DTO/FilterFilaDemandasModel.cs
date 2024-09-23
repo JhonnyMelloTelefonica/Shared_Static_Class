16e4d1163d0c1497bd062161c6e7b0423979007c
JHONNY GLAUCIO NASCIMENTO DE MELO <jhonny.melo@telefonica.com>
2024-09-23 11:52:46 -0300

    Correções de filtro em Controle de Demandas

--- Model_DTO/FilterFilaDemandasModel.cs
+++ Model_DTO/FilterFilaDemandasModel.cs
@@ -9,17 +9,19 @@ namespace Shared_Static_Class.Model_DTO
 
     public class RELACAO_FILA_SUB_FILA
     {
-        public RELACAO_FILA_SUB_FILA(int iD_FILA, string nOME_FILA, int iD_SUB_FILA, string nOME_SUB_FILA)
+        public RELACAO_FILA_SUB_FILA(int iD_FILA, string nOME_FILA, int iD_SUB_FILA, string nOME_SUB_FILA, int[] responsaveis = null)
         {
             ID_FILA = iD_FILA;
             NOME_TIPO_FILA = nOME_FILA;
             ID_SUB_FILA = iD_SUB_FILA;
             NOME_SUB_FILA = nOME_SUB_FILA;
+            this.responsaveis = responsaveis ?? [];
         }
 
         public int ID_FILA { get; set; }
         public string NOME_TIPO_FILA { get;set;}
         public int ID_SUB_FILA {get;set;}
         public string NOME_SUB_FILA {get;set;}
+        public IEnumerable<int> responsaveis {get;set;}
     }
 }
