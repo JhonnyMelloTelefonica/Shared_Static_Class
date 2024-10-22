3fbf7ca8995d32800af444676c51dbdfcf428758
JHONNY GLAUCIO NASCIMENTO DE MELO <jhonny.melo@telefonica.com>
2024-10-22 10:13:02 -0300

    Finalização do Fórum GiroV; Construção do NavMenu a partir de arquivo Json;

---
+++ Model/NavMenuData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared_Static_Class.Model;
+public class NavMenuData
+{
+    public string Application { get; set; }
+    public string Título { get; set; }
+    public Linkbase LinkBase { get; set; }
+    public Link[] Links { get; set; }
+    public string Permission { get; set; }
+}
+
+public class Linkbase
+{
+    public string linkDev { get; set; }
+    public string linkProduction { get; set; }
+}
+
+public class Link
+{
+    public string Name { get; set; }
+    public string LinkNavigation { get; set; }
+    public string Permission { get; set; }
+}
