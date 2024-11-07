982c6e6286648b69bc44fd8438a7f4faaae31d46
JHONNY GLAUCIO NASCIMENTO DE MELO <jhonny.melo@telefonica.com>
2024-11-07 14:59:03 -0300

    Funcionalidade de Encaminhamento de Fila;

--- Model/RespostaFechamento.cs
+++ Model/RespostaFechamento.cs
@@ -12,6 +12,7 @@ namespace Shared_Razor_Components.FundamentalModels
         public int MATRICULA { get; set; }
         public Guid ID_RELACAO { get; set; }
         public int? MATRICULA_REDIRECIONADO { get; set; }
+        public int? SUBFILA { get; set; }
         public DateTime Data { get; set; }
         public string Status { get; set; } = string.Empty;
         public List<FileDataModel> Arquivos { get; set; } = new();
