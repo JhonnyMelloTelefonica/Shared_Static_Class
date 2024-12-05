19220c1acc5e92c0ec8079a53e74b0d3ed60edc7
AdrielFnd <adriel.paula@telefonica.com>
2024-12-05 15:21:50 -0300

    alterações filtro filas

--- Model_DTO/PaginationFilaDemandasModel.cs
+++ Model_DTO/PaginationFilaDemandasModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Drawing;
+using System.Text.Json.Serialization;
 
 namespace Shared_Static_Class.Model_DTO
 {
@@ -8,8 +9,23 @@ namespace Shared_Static_Class.Model_DTO
         public string nome_fila { get; set; } = string.Empty;
         public IReadOnlyList<string> regional { get; set; } = [];
         public List<string> campos { get; set; } = [];
-        public List<int> responsável { get; set; }  = [];
-        public List<int> fila_macro { get; set; }  = [];
-        public List<int> sub_fila { get; set; }  = [];
+        public List<int> responsável { get; set; } = [];
+        public List<int> fila_macro { get; set; } = [];
+        public List<int> sub_fila { get; set; } = [];
+        public List<int> idSub { get; set; } = [];
+        public List<bool> status { get; set; } = [true];
+
+        public class STATUS
+        {
+            public string? NomeStatus { get; set; }
+            public bool? valor { get; set; }
+        }
+       public List<STATUS> statusOptions = new List<STATUS>
+        {
+         new STATUS{ NomeStatus="ABERTO", valor=true},
+         new STATUS{ NomeStatus="FECHADO", valor=false}
+        };
+
+        
     }
 }
